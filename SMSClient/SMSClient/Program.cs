29254e654ef5ca47b22548d208598a45705acdc5
Stephen kenny <stephenkenny@outlook.ie>
2015-02-13 13:00:03 +0000

    SMS lan done
    Test and working.

---
+++ SMSClient/SMSClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSClient
+{
+    class Program
+    {
+            static async Task RunAsync()
+            {
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("http://localhost:5973/");                             // base URL for API Controller i.e. RESFul service
+
+                        // add an Accept header 
+                        client.DefaultRequestHeaders.
+                                Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));         // or application/xml or application/bson
+
+
+                        // POST /api/SMSGateway with a txt message serialised in request body
+                        // send a txt msg
+                        TextMessage txt = new TextMessage() { FromNumber = "087 1111111", Content = "Hello :-)", ToNumber = "087 2222222" };
+                        HttpResponseMessage response = await client.PostAsJsonAsync("api/SMS", txt);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("Message sent ok");
+                        }
+                        else
+                        {
+                            Console.WriteLine(response.StatusCode + " " + response.ReasonPhrase);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            // kick off
+            static void Main()
+            {
+                Task result = RunAsync();
+                result.Wait();
+                Console.ReadLine();
+            }
+        }
+    }
+
